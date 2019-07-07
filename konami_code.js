e217adcf8a628169db1cc764aa16d6b3d2860ce3
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 11:31:06 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,7 @@ const codes = [
 
 function init() {
   // your code here
-/*
+
   let i = 0
 
   document.body.addEventListener('keydown', (e) => {
@@ -30,30 +30,3 @@ function init() {
     }
   })
 }
-
-init()
-*/
-
-let index = 0;
-
-document.body.addEventListener("keydown", (thingPressed) => {
-  const key = thingPressed.key
-  console.log(key)
-
-  if (codes[index] == key) {
-    ++index
-  }
-
-  else {
-    idx = 0
-  }
-
-  console.log(index)
-  if (index == 10) {
-    alert("Hurray!");
-    index = 0
-  }
-});
-}
-
-init()
