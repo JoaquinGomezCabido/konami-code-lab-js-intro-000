e0455c7ead075e68f57bad4d44cec12132d0da98
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 11:30:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,7 @@ const codes = [
 
 function init() {
   // your code here
-
+/*
   let i = 0
 
   document.body.addEventListener('keydown', (e) => {
@@ -28,21 +28,32 @@ function init() {
       alert('Congratulations, you have unlocked the Konami Code!')
       i = 0
     }
-
-    /*
-    for(var i = 0; i < 10; i++) {
-      if(e.key === codes[i]) {
-        if(i === 9) {
-          alert('Congratulations, you have unlocked the Konami Code!')
-        } else {
-          continue
-        }
-      } else {
-        break
-      }
-    }
-    */
   })
+}
+
+init()
+*/
+
+let index = 0;
+
+document.body.addEventListener("keydown", (thingPressed) => {
+  const key = thingPressed.key
+  console.log(key)
+
+  if (codes[index] == key) {
+    ++index
+  }
+
+  else {
+    idx = 0
+  }
+
+  console.log(index)
+  if (index == 10) {
+    alert("Hurray!");
+    index = 0
+  }
+});
 }
 
 init()
